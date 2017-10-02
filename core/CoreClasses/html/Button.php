23da3a8fc6d8ea0549d627e0264aa76bda146a38
Hadi AmirNahavandi <Hadi.nahavandi2010@gmail.com>
2017-10-02 04:13:14 +0330

    Added GlyphiconClass and DisplayMode.

--- core/CoreClasses/html/Button.php
+++ core/CoreClasses/html/Button.php
@@ -4,8 +4,29 @@ namespace core\CoreClasses\html;
 
 class Button extends baseHTMLElement {
 	private $class,$name,$text,$id,$isSubmit,$type;
+    private $glyphiconClass;
+    private $DisplayMode;
+
+    /**
+     * @param int $DisplayMode
+     */
+    public function setDisplayMode($DisplayMode)
+    {
+        $this->DisplayMode = $DisplayMode;
+    }
+    public static $DISPLAYMODE_INPUT=1;
+    public static $DISPLAYMODE_BUTTON=2;
+
+    /**
+     * @param mixed $glyphiconClass
+     */
+    public function setGlyphiconClass($glyphiconClass)
+    {
+        $this->glyphiconClass = $glyphiconClass;
+    }
 	function __construct($issubmit=true,$text="Button",$name=null,$id=null,$class=null)
 	{
+	    $this->setDisplayMode(Button::$DISPLAYMODE_INPUT);
 		$this->isSubmit=$issubmit;
 		
 		if(is_null($name))
@@ -14,8 +35,9 @@ class Button extends baseHTMLElement {
 			$id=$name;
 		if(is_null($class))
 			$class=$id;
-		
-		
+
+
+        $this->setGlyphiconClass(null);
 		$this->setName($name);
 		$this->SetAttribute("value", $text);
 		$this->setId($id);
@@ -24,12 +46,27 @@ class Button extends baseHTMLElement {
 	public function getHTML()
 	{
 
+        $glyphicon="";
+        if($this->glyphiconClass!=null)
+            $glyphicon="<i class='" . $this->glyphiconClass . "'></i>";
 		if(!$this->isSubmit)
 			$this->setType("button");
 		else
 			$this->setType("submit");
-		$html="<input ";
-		$html.=$this->getAttributesDefinition() . " />";
+		if($this->DisplayMode==Button::$DISPLAYMODE_INPUT)
+        {
+            $html="<input ";
+            $html.=$this->getAttributesDefinition() . " />";
+        }
+        else
+        {
+            $html="<button ";
+            $html.=$this->getAttributesDefinition() . " >";
+            if($glyphicon!="")
+                $html.="\n " . $glyphicon;
+            $html.="\n " . $this->getAttribute("value");
+            $html.="</button>";
+        }
 		return $html;
 	}
 
